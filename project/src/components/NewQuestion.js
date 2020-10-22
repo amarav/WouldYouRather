1eb47a0aa1ac106ff2ec10025da01946ce946e5b
amarav <amara.sudha@gmail.com>
2020-10-22 09:50:18 +0000

    Newques styled

--- project/src/components/NewQuestion.js
+++ project/src/components/NewQuestion.js
@@ -1,52 +1,52 @@
-import React,{Component} from 'react';
-import { Col, Form, FormGroup, Label, Input , Button} from 'reactstrap';
-import {addNewQuestion} from '../actions/questions'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import React, { Component } from "react";
+import { Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { addNewQuestion } from "../actions/questions";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-class NewQuestion extends Component{
-  
+class NewQuestion extends Component {
   state = {
-     optionOneText:'',
-     optionTwoText:'',
-     gotoHome:false,
-  }
-  
+    optionOneText: "",
+    optionTwoText: "",
+    gotoHome: false,
+  };
+
   handleInputChange = (event) => {
-    event.preventDefault()
-    const {id,value} = event.target
-    this.setState( () => ({[id]:[value]}))
-  }
+    event.preventDefault();
+    const { id, value } = event.target;
+    this.setState(() => ({ [id]: [value] }));
+  };
 
   handleSubmit = (event) => {
-    const {optionOneText,optionTwoText} = this.state
-    event.preventDefault()
-    this.props.addQuestion(optionOneText,optionTwoText)
-    this.setState({gotoHome:true})    
-  }
+    const { optionOneText, optionTwoText } = this.state;
+    event.preventDefault();
+    this.props.addQuestion(optionOneText, optionTwoText);
+    this.setState({ gotoHome: true });
+  };
 
-  render(){
-    
-   const {optionOneText,optionTwoText,gotoHome } = this.state
-   if(!this.props.authedUser){
-     return <Redirect to={{
-                            pathname :"/login",
-                            state : {
-                                     returnPath:'/new'
-                            }
-                         }}
-            />      
-   }
+  render() {
+    const { optionOneText, optionTwoText, gotoHome } = this.state;
+    if (!this.props.authedUser) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: {
+              returnPath: "/new",
+            },
+          }}
+        />
+      );
+    }
 
-   if(gotoHome === true) {
-      return <Redirect to='/home' />
-   }
-    
-  return (
-    <div>
-    <Form className="col-md-4 offset-md-4" onSubmit={this.handleSubmit}>  
+    if (gotoHome === true) {
+      return <Redirect to="/home" />;
+    }
 
-      <section className="page-section bg-light">
+    return (
+      <div>
+        <Form className="col-md-4 offset-md-4" onSubmit={this.handleSubmit}>
+          <section className="page-section bg-light">
             <div className="container-fluid">
               <div className="text-center">
                 <h3 className="section-heading text-uppercase">
@@ -54,49 +54,61 @@ class NewQuestion extends Component{
                 </h3>
                 <h3 className="section-subheading text-muted">
                   Complete the question
-                </h3>                
-              
-                <h3 className="section-heading">
-                  Would You Rather...
                 </h3>
-                </div>
+                <h3 className="section-heading">Would You Rather...</h3>
+              </div>
             </div>
-    <br/>
- 
-      <FormGroup row>
-        <Col className="offset-md-1" sm={10}>
-          <Input type="text"  id="optionOneText"  placeholder="Enter Option one text here" 
-onChange={this.handleInputChange}/>
-        </Col>
-      </FormGroup>
-      <Label className="text-center offset-md-5" >OR</Label>
-      <FormGroup row>
-        <Col className="offset-md-1" sm={10}>
-          <Input type="text"  id="optionTwoText" placeholder="Enter Option two text here" 
-onChange={this.handleInputChange}/>
-        </Col>
-      </FormGroup>
-<br/>
-         <Button color="secondary" size="lg" block disabled={optionOneText==='' || optionTwoText===''}>Submit</Button>
+            <br />
+
+            <FormGroup row>
+              <Col className="offset-md-1" sm={10}>
+                <Input
+                  type="text"
+                  id="optionOneText"
+                  placeholder="Enter Option one text here"
+                  onChange={this.handleInputChange}
+                />
+              </Col>
+            </FormGroup>
+            <Label className="text-center offset-md-5">OR</Label>
+            <FormGroup row>
+              <Col className="offset-md-1" sm={10}>
+                <Input
+                  type="text"
+                  id="optionTwoText"
+                  placeholder="Enter Option two text here"
+                  onChange={this.handleInputChange}
+                />
+              </Col>
+            </FormGroup>
+            <br />
+            <Button
+              color="secondary"
+              size="lg"
+              block
+              disabled={optionOneText === "" || optionTwoText === ""}
+            >
+              Submit
+            </Button>
           </section>
-</Form>
-</div>
-  );
- }
-}
-
-function mapStateToProps({authedUser}) {
-  return {   
-    authedUser,
-  }  
-}
-
-function mapDispatchToProps(dispatch){
-  return {
-    addQuestion : (optionOnetext,optionTwotext) => {
-         dispatch(addNewQuestion(optionOnetext,optionTwotext))
-    }
+        </Form>
+      </div>
+    );
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(NewQuestion)
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addQuestion: (optionOnetext, optionTwotext) => {
+      dispatch(addNewQuestion(optionOnetext, optionTwotext));
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewQuestion);
