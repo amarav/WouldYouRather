f211969f224aae107e6446ba3259fcb45e7538f0
amarav <amara.sudha@gmail.com>
2020-10-22 10:22:30 +0000

    Unanswered questions styled

--- project/src/components/Questions.js
+++ project/src/components/Questions.js
@@ -1,48 +1,52 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {Link,Redirect} from 'react-router-dom'
+import { Link, Redirect } from "react-router-dom";
 
 class Question extends Component {
-
   render() {
-    const { question,author,id,authedUser } = this.props;
-    if(!authedUser)
-    {
-      return <Redirect to={ { pathname:'/login',
-                              state: {
-                               returnPath:'/questions'
-                              }   
-                          }}/>
+    const { question, author, id, authedUser } = this.props;
+    if (!authedUser) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: {
+              returnPath: "/questions",
+            },
+          }}
+        />
+      );
     }
-    
+
     return (
-      <div>      
-        
+      <div>
         <div className="card">
-          <div className="fullcard-image">
-            <div
-              className="fullcard-avatar"
-              style={{ backgroundImage: `url(${author.avatarURL})` }}/>
+          <div class="myrow">
+            <div class="mycolumn">
+              <div
+                className="fullcard-avatar"
+                style={{ backgroundImage: `url(${author.avatarURL})` }}
+              />
+            </div>
+            <div class="mycolumn">
+              <div className="card-body text-center">
+                <h2 className="font-italic">{question.author} asks...</h2>
+                <p className="font-weight-bold">Would you Rather...</p>
+                <label>{question.optionOne.text}</label>
+                <br /> OR <br />
+                <label>{question.optionTwo.text}</label>
+                <br />
+              </div>
+            </div>
+            <div class="mycolumn">
+              <div className="card-body text-center">
+                <Link to={`/questions/${id}`}>
+                  <label>Answer question</label>
+                </Link>{" "}
+              </div>
+            </div>
           </div>
-          <div className="card-body text-center">
-            <p className="font-italic">{question.author} asks...</p>
-            <p className="font-weight-bold">Would you Rather...</p>            
-              <label>
-                 {question.optionOne.text}
-              </label>
-                <br/>  OR  <br/>
-              <label>
-                {question.optionTwo.text}
-              </label>  
-<br/>
-        <Link to={`/questions/${id}`}>
-<label>
-Answer question
-</label>
-</Link>       
-</div>
-        </div>    
-       
+        </div>
       </div>
     );
   }
