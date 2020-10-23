72b98e9fcf47500725ede41ba67a2996ae2cb005
amarav <amara.sudha@gmail.com>
2020-10-23 14:29:52 +0000

    question details styled

--- project/src/components/MissingErr.js
+++ project/src/components/MissingErr.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MissingErr() {
   return (
     <div>
       <h1>ERROR 404: No questions found !!! </h1>
+      <br/>
+      <Link to='/home'>Go Home</Link>
     </div>
   );
 }
--- project/src/components/QuestionDetails.js
+++ project/src/components/QuestionDetails.js
@@ -1,8 +1,8 @@
-import React,{Component} from 'react'
-import {connect} from 'react-redux'
-import {answerQuestion} from '../actions/questions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { answerQuestion } from "../actions/questions";
 
-class QuestionDetails extends Component{
+class QuestionDetails extends Component {
   constructor(props) {
     super(props);
 
@@ -11,76 +11,88 @@ class QuestionDetails extends Component{
       selectedOption: "optionOne",
     };
   }
-  
+
   handleOptionChange(changeEvent) {
     this.setState({
       selectedOption: changeEvent.target.value,
-    });    
+    });
   }
-  
+
   SubmitVote = (event) => {
-    const {dispatch} = this.props
-    dispatch(answerQuestion(this.props.id,this.state.selectedOption))
-    this.props.history.push('/home')
-  }
-  
-  render(){    
-    const {question,users} = this.props
-    return(
-      <div className="col-md-8 offset-md-3">
-      {question ? 
-       <div className="mysmcard">
-          <div className="mysmcard-image">
-            <div
-              className="myimg-thumbnail"
-              style={{ backgroundImage: `url(${users[question.author].avatarURL})` }}/>
-          </div>
-          <div className="card-body text-center">
-            <p className="font-italic">{question.author} asks...</p>
-            <p className="font-weight-bold">Would you Rather...</p>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="optionOne"
-                  checked={this.state.selectedOption === "optionOne"}
-                  onChange={this.handleOptionChange}
+    const { dispatch } = this.props;
+    dispatch(answerQuestion(this.props.id, this.state.selectedOption));
+    this.props.history.push("/home");
+  };
+
+  render() {
+    const { question, users } = this.props;
+    return (
+      <div className="col-md-8 offset-md-2">
+        {question ? (
+          <div className="card">
+            <div className="myrow">
+              <div className="mycolumn">
+                {" "}
+                <div
+                  className="fullcard-avatar"
+                  style={{
+                    backgroundImage: `url(${users[question.author].avatarURL})`,
+                  }}
                 />
-                {question.optionOne.text}
-              </label>
+              </div>
+              <div className="mycolumn">
+                <div className="card-body text-center">
+                  <p className="font-italic">{question.author} asks...</p>
+                  <p className="font-weight-bold">Would you Rather...</p>
+                  <div className="radio">
+                    <label>
+                      <input
+                        type="radio"
+                        value="optionOne"
+                        checked={this.state.selectedOption === "optionOne"}
+                        onChange={this.handleOptionChange}
+                      />
+                      {question.optionOne.text}
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label>
+                      <input
+                        type="radio"
+                        value="optionTwo"
+                        checked={this.state.selectedOption === "optionTwo"}
+                        onChange={this.handleOptionChange}
+                      />
+                      {question.optionTwo.text}
+                    </label>
+                  </div>
+                  <button
+                    className="btn btn-info"
+                    type="submit"
+                    onClick={this.SubmitVote}
+                  >
+                    VOTE
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="optionTwo"
-                  checked={this.state.selectedOption === "optionTwo"}
-                  onChange={this.handleOptionChange}
-                />
-                {question.optionTwo.text}
-              </label>
-            </div>
-            <button className="btn btn-info" type="submit" onClick={this.SubmitVote}>
-              VOTE
-            </button>
           </div>
-        </div> 
-      :  this.props.history.push('/MissingErr') }
+        ) : (
+          this.props.history.push("/MissingErr")
+        )}
       </div>
-    )
+    );
   }
 }
 
-
-function mapStatetoProps({questions,users},props)
-{
-  const id = props.match.params.id.toString()
+function mapStatetoProps({ questions, users }, props) {
+  const id = props.match.params.id.toString();
   const question = questions[id];
   return {
     question,
     id,
-    users    
-  }
+    users,
+  };
 }
 
-export default connect(mapStatetoProps)(QuestionDetails)
+export default connect(mapStatetoProps)(QuestionDetails);
