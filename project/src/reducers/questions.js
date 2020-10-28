9e46065b9a3bdf936c4fae835f10782a0e416eeb
amarav <amara.sudha@gmail.com>
2020-10-28 06:45:52 +0000

    action types moved to separate file

--- project/src/reducers/questions.js
+++ project/src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS,ADD_QUESTION, SAVE_QUESTION } from '../actions/questions'
+import { RECEIVE_QUESTIONS,ADD_QUESTION, SAVE_QUESTION } from '../actions/types'
 
 export default function questions (state = {}, action) {
   switch(action.type) {
